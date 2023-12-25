58c58177ad20f45d05b1c36991786c6468712d43
Weldon Carlos <weldonprogrammer2022@gmail.com>
2023-12-25 16:21:20 -0300

    Aprimoramento dos textos exemplos da descrição dos cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Cards from "../../components/cards/Cards";
-import { Box } from "@mui/material";
-import imagemCard1 from '../../assets/imagensCards/imagemCard1.jpg'
-import imagemCard2 from '../../assets/imagensCards/imagemCard2.jpg'
-import imagemCard3 from '../../assets/imagensCards/imagemCard3.jpg'
+import { Box, Grid } from "@mui/material";
+import imagemCard1 from "../../assets/imagensCards/imagemCard1.jpg";
+import imagemCard2 from "../../assets/imagensCards/imagemCard2.jpg";
+import imagemCard3 from "../../assets/imagensCards/imagemCard3.jpg";
+import "./Home.css";
+
 const Home = () => {
   const informacoesCards = [
     {
@@ -11,31 +13,37 @@ const Home = () => {
       imagem: imagemCard1,
       titulo: "Serviços",
       descricao:
-        "Todos os nossos serviços estão disponives para melhor lhe atender, clique em saiba mais e contrate um de nossos serviços",
+        "Descubra a excelência em serviços da RN Terraplanagem! Estamos aqui para melhor atender você... ",
     },
     {
       id: 2,
       imagem: imagemCard2,
       titulo: "Obras",
       descricao:
-        "Confira todas as nossas obras com apenas alguns cliques,bbsbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+        "Explore o mundo da construção e transformação na RN soluções completas para cada projeto...",
     },
     {
       id: 3,
       imagem: imagemCard3,
       titulo: "Empresa",
       descricao:
-        "Conheça a nossa historia toda a nossa trajetoria até chegar aqui, e também um pouco das nossas politicas de preservação ",
+        "Conheça a RN - Transformando Ideias em Realidade! Somos uma empresa comprometida em..",
     },
   ];
   return (
-    <Box display={'flex'} gap={'25px'} justifyContent={'center'} >
+
+    <Grid display={'flex'} justifyContent={"center"}  container spacing={2}>
+
       {informacoesCards.map((item) => (
-        <Box key={item.id}  >
-          <Cards titulo={item.titulo} descricao={item.descricao} imagem={item.imagem} />
-        </Box>
+        <Grid display={'flex'} justifyContent={"center"} item xs={12} sm={3} md={3} lg={3} margin={'50px 0 0 0'}>
+          <Cards
+            titulo={item.titulo}
+            descricao={item.descricao}
+            imagem={item.imagem}
+          />
+        </Grid>
       ))}
-    </Box>
+    </Grid>
   );
 };
 
