c7dae6041acbe296e1cf0c3da6e5d806f14e63a9
Weldon Carlos <weldonprogrammer2022@gmail.com>
2023-12-26 10:04:17 -0300

    Montando a estrutura do componente

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            <h2>Endereço</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.quo doloremque</p>
+        </div>
+        <div>
+           <h2>Contato</h2>
+           <p>(48)99998-8822</p> 
+        </div>
+    </div>
+  )
+}
+
+export default Footer
