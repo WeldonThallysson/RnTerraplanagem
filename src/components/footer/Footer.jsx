5e885fb94ad35ffbef30c495fac4ec6e97d06ee4
Weldon Carlos <weldonprogrammer2022@gmail.com>
2023-12-27 14:22:07 -0300

    Alteração no style in line do componente footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,9 +20,9 @@ const Footer = () => {
   return (
     <Box sx={{ flexGrow: 1, } }>
 
-      <AppBar position="static" sx={{ backgroundColor: " #252525",  height:''}}>
+      <AppBar position="static" sx={{ backgroundColor: " #252525",  marginTop:'100px'}}>
 
-        <StyledToolbar sx={{ display: "flex", justifyContent: "center", alignItems:'flex-end',marginTop:'-70px' }}>
+        <StyledToolbar sx={{ display: "flex", justifyContent: "center", alignItems:'flex-end',marginTop:'-60px' }}>
           <Box
             sx={{
               display: "flex",
